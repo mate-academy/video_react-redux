f70bfd546951701a23d63bd8d5ce8218f3c977fd
Piotr <piotrkrasucki@gmail.com>
2023-11-27 13:44:40 +0100

    initial

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,17 @@
 // npm i redux @types/redux
 import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import amount from '../features/amount';
+import goods from '../features/goods';
+import position from '../features/position';
 
-const reducer = combineReducers({});
-const store = createStore(reducer);
+const reducer = combineReducers({
+    amount,
+    goods,
+    position,
+});
+const store = createStore(reducer, composeWithDevTools());
+
+export type RootState = ReturnType<typeof store.getState>
 
 export default store;
